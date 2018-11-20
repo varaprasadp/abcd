89397cf4b63c9c2cdccc1b6dd997749f4fa59b89
varaprasadp <vara199715@gmail.com>
2018-11-20 14:38:18 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def connect():
-    conn = sqlite3.connect('postgres://gwvshgjceutpcf:6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d@ec2-54-235-156-60.compute-1.amazonaws.com:5432/d10vqpdr1b33rr')
+    conn = sqlite3.connect('d10vqpdr1b33rr')
     conn.execute('CREATE TABLE IF NOT EXISTS database (name TEXT, age INTEGER, gender TEXT)')
     conn.close()
 
