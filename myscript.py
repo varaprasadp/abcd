43b860ae66b2258417d3d89bfea7f5d35df1ed10
varaprasadp <vara199715@gmail.com>
2018-11-20 19:30:33 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,19 +1,17 @@
 import psycopg2
 import urllib.parse as urlparse
 import os
-def conn():
-            url = urlparse.urlparse(os.environ['DATABASE_URL'])
-            dbname = url.path[1:]
-            user = url.username
-            password = url.password
-            host = url.hostname
-            port = url.port
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
 
-            con = psycopg2.connect(
-                        dbname=dbname,
-                        user=user,
-                        password=password,
-                        host=host,
-                        port=port
-                        )
-            return(con)
+con = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
