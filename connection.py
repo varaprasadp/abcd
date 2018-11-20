53936551f6b303c590cf4ac6681372545bb6ab6b
varaprasadp <vara199715@gmail.com>
2018-11-20 17:21:55 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import psychopg2
 import psychopg2.extras
 
 def connect():
-    conn = ('database=d10vqpdr1b33rr user=gwvshgjceutpcf password = 6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d host= ec2-54-235-156-60.compute-1.amazonaws.com')
+    conn = ('dbname=database user=gwvshgjceutpcf password = 6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d host= ec2-54-235-156-60.compute-1.amazonaws.com')
     try:
         return psychopg2.connect(conn)
     except:
