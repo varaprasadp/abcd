28ecd5a4f6ebbcb281ecdd3f2c495e28083ceecc
varaprasadp <vara199715@gmail.com>
2018-11-20 19:35:12 +0530

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -40,5 +40,4 @@ def data():
    return render_template("data.html",rows = rows)
 
 if __name__ == '__main__':
-   app.run(debug = True, use_reloader = True")
-   
+   app.run(debug = True, use_reloader = True)
