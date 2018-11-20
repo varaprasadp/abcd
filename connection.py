18f34fb0091e5c7113906205c6d736458952909b
varaprasadp <vara199715@gmail.com>
2018-11-20 17:59:20 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,5 +1,5 @@
-import psychopg2
-import psychopg2.extras
+import psycopg2
+import psycopg2.extras
 
 def connect():
     conn = ("dbname='d4669f4pa3dkk8' user='gwvshgjceutpcf' password='6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d' host='ec2-54-235-156-60.compute-1.amazonaws.com' port='5432'")
