0a8abd2bee321886ed6ca601d32318cfb6211fe0
varaprasadp <vara199715@gmail.com>
2018-11-20 18:58:49 +0530

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -38,6 +38,11 @@ def data():
    return render_template("data.html",rows = rows)
 
 if __name__ == '__main__':
+    conn = ("dbname='d4669f4pa3dkk8' user='gwvshgjceutpcf' password='6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d' host='ec2-54-235-156-60.compute-1.amazonaws.com' port='5432'")
+    try:
+        return psycopg2.connect(conn)
+    except:
+        print('cannot connect')
    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
    cur=con.cursor()
    cur.execute("CREATE TABLE IF NOT EXISTS database (name varchar(20), age integer, gender varchar(20));")
