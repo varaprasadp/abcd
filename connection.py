fb31ee28ffc9da45e2b95111fb80f18f337780e1
varaprasadp <vara199715@gmail.com>
2018-11-21 11:44:43 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,5 +1,5 @@
 import sqlite3
  def connect():
-    conn = sqlite3.connect('ec2-54-235-156-60.compute-1.amazonaws.com')
+    conn = sqlite3.connect()
     conn.execute('CREATE TABLE IF NOT EXISTS database (name TEXT, age INTEGER, gender TEXT)')
     conn.close()
