a4aa7f3f8695a43a3fc62952e00fd79e448f38b6
varaprasadp <vara199715@gmail.com>
2018-11-21 11:44:21 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,9 +1,5 @@
-import psycopg2
-import psycopg2.extras
-
-def connect():
-    conn = ("dbname='d4669f4pa3dkk8' user='gwvshgjceutpcf' password='6405b053099f7ae42afde62c180253c7a86cb43d3e134eb5a3366aee4a4ee47d' host='ec2-54-235-156-60.compute-1.amazonaws.com' port='5432'")
-    try:
-        return psycopg2.connect(conn)
-    except:
-        print('cannot connect')
+import sqlite3
+ def connect():
+    conn = sqlite3.connect('ec2-54-235-156-60.compute-1.amazonaws.com')
+    conn.execute('CREATE TABLE IF NOT EXISTS database (name TEXT, age INTEGER, gender TEXT)')
+    conn.close()
