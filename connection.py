62d96b7f4b75b17e9d706b6152f50b29b35c3c66
varaprasadp <vara199715@gmail.com>
2018-11-21 10:21:00 +0530

    Delete connection.py

--- connection.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-def connect():
-    conn = sqlite3.connect('ec2-54-235-156-60.compute-1.amazonaws.com')
-    conn.execute('CREATE TABLE IF NOT EXISTS database (name TEXT, age INTEGER, gender TEXT)')
-    conn.close()
-
